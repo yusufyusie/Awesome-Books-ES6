6c9f6939abd722fc9f7acc7ffdb81765937d4c3c
yusufyusie <yesuffenta023@gmail.com>
2023-07-11 21:59:13 +0300

    Add local storege file

--- modules/Book.js
+++ modules/Book.js
@@ -6,47 +6,4 @@ class Book {
       this.author = author;
       this.id = id;
     }
-  
-    static storeBooks() {
-      let books;
-      const storeData = localStorage.getItem('local');
-      if (!storeData) {
-        books = [];
-      } else {
-        books = JSON.parse(storeData);
-      }
-      return books;
-    }
-  
-    static addBooks(addNewBook) {
-      const books = Book.storeBooks();
-      if (books) {
-        books.push(addNewBook);
-        localStorage.setItem('local', JSON.stringify(books));
-      }
-    }
-  
-    static showBooks() {
-      const books = Book.storeBooks();
-      books.forEach((addNewBook) => {
-        Book.addBookToList(addNewBook);
-      });
-    }
-  
-    static addBookToList(addNewBook) {
-      addNewBook.id = newId;
-      const bookInfo = `
-        <div id="${newId}">
-          <p>"<span>${addNewBook.title}</span>" by
-          <span class="">${addNewBook.author}</span></p>
-          <button id="${newId}" class="remove-btn">Remove</button>
-        </div>
-      `;
-      bookDetail.innerHTML += bookInfo;
-      newId += 1;
-    }
-  
-    static deleteBookFromList(e) {
-      e.parentElement.remove();
-    }
   }
---
+++ modules/LocalStore.js
@@ -0,0 +1,24 @@
+class LocalStore {
+    constructor() {
+        this.newId = this.storeBooks().length + 1;
+
+    }
+    static storeBooks() {
+        let books;
+        const storeData = localStorage.getItem('local');
+        if (!storeData) {
+          books = [];
+        } else {
+          books = JSON.parse(storeData);
+        }
+        return books;
+      }
+    
+      static addBooks(addNewBook) {
+        const books = LocalStore.storeBooks();
+        if (books) {
+          books.push(addNewBook);
+          localStorage.setItem('local', JSON.stringify(books));
+        }
+      }
+}
